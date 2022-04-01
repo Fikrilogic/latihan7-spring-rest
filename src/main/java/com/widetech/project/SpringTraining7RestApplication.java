52c7d048b9e39375acc6eef21e06d93fce8411e4
fikrilogic <fikrisandi3074@gmail.com>
2022-04-01 15:17:25 +0700

    ïnitializing project

---
+++ src/main/java/com/widetech/project/SpringTraining7RestApplication.java
@@ -0,0 +1,13 @@
+package com.widetech.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTraining7RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTraining7RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/widetech/project/SpringTraining7RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widetech.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTraining7RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
